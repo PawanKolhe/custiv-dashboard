7f023e15c7360bea1a5298f69c2aad00db257814
Pawan Kolhe <contact@pawankolhe.com>
2021-03-27 14:53:43 +0530

    style: setup global styles

--- src/pages/Suppliers/Suppliers.js
+++ src/pages/Suppliers/Suppliers.js
@@ -1,9 +1,14 @@
 import styles from './suppliers.module.scss'
+import PageHeader from '../../components/PageHeader/PageHeader'
 
 export default function Suppliers() {
   return (
     <div className={styles.Suppliers}>
-      Suppliers
+      <PageHeader title="Suppliers" />
+
+      <div className="container page-content">
+        Page
+      </div>
     </div>
   )
 }
