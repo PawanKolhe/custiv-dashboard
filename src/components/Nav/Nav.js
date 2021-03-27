b55ca5a984d7dd7d04656d3a07824467927d6857
Pawan Kolhe <contact@pawankolhe.com>
2021-03-27 14:54:59 +0530

    feat: nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,44 @@
 import styles from './nav.module.scss'
 import { NavLink } from "react-router-dom";
+import { InlineIcon } from '@iconify/react-with-api';
 
 export default function Nav() {
   return (
     <div className={styles.Nav}>
-        <NavLink to="/quotes">
-          <div className={styles.Nav__item}>
-            <div className={styles.Nav__itemIcon}>Icon</div>
-            <div className={styles.Nav__itemText}>Quotes</div>
+        <NavLink to="/quotes" className={styles.Nav__item} activeClassName={styles.Nav__itemActive}>
+          <div className={styles.Nav__itemIcon}>
+            <InlineIcon icon="ph:note" className={styles.Nav__itemIconInactive} />
+            <InlineIcon icon="ph:note-fill" className={styles.Nav__itemIconActive} />
           </div>
+          <div className={styles.Nav__itemText}>Quotes</div>
         </NavLink>
-        <NavLink to="/projects">
-          <div className={styles.Nav__item}>
-            <div className={styles.Nav__itemIcon}>Icon</div>
-            <div className={styles.Nav__itemText}>Projects</div>
+        <NavLink to="/projects" className={styles.Nav__item} activeClassName={styles.Nav__itemActive}>
+          <div className={styles.Nav__itemIcon}>
+            <InlineIcon icon="ph:package" className={styles.Nav__itemIconInactive} />
+            <InlineIcon icon="ph:package-fill" className={styles.Nav__itemIconActive} />
           </div>
+          <div className={styles.Nav__itemText}>Projects</div>
+        </NavLink>
+        <NavLink to="/messages" className={styles.Nav__item} activeClassName={styles.Nav__itemActive}>
+          <div className={styles.Nav__itemIcon}>
+            <InlineIcon icon="ph:chat-centered-dots" className={styles.Nav__itemIconInactive} />
+            <InlineIcon icon="ph:chat-centered-dots-fill" className={styles.Nav__itemIconActive} />
+          </div>
+          <div className={styles.Nav__itemText}>Messages</div>
+        </NavLink>
+        <NavLink to="/suppliers" className={styles.Nav__item} activeClassName={styles.Nav__itemActive}>
+          <div className={styles.Nav__itemIcon}>
+            <InlineIcon icon="ph:truck" className={styles.Nav__itemIconInactive} />
+            <InlineIcon icon="ph:truck-fill" className={styles.Nav__itemIconActive} />
+          </div>
+          <div className={styles.Nav__itemText}>Suppliers</div>
+        </NavLink>
+        <NavLink to="/clients" className={styles.Nav__item} activeClassName={styles.Nav__itemActive}>
+          <div className={styles.Nav__itemIcon}>
+            <InlineIcon icon="ph:users" className={styles.Nav__itemIconInactive} />
+            <InlineIcon icon="ph:users-fill" className={styles.Nav__itemIconActive} />
+          </div>
+          <div className={styles.Nav__itemText}>Clients</div>
         </NavLink>
     </div>
   )
