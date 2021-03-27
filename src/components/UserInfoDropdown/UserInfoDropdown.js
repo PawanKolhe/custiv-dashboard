9284d0d1ff48fe019a48f32ce5f1368c406bacdd
Pawan Kolhe <contact@pawankolhe.com>
2021-03-27 22:58:53 +0530

    feat: add chevron

--- src/components/UserInfoDropdown/UserInfoDropdown.js
+++ src/components/UserInfoDropdown/UserInfoDropdown.js
@@ -1,24 +1,30 @@
 import { forwardRef } from 'react';
 import styles from './userInfoDropdown.module.scss'
+import { InlineIcon } from '@iconify/react-with-api';
 
 const UserInfoDropdown = ({ img }, ref) => {
   return (
     <div className={styles.UserInfo} ref={ref}>
-      <div className={styles.UserInfo__image}>
-        {img ?
-          <img src={img} alt="Profile" />
-          :
-          <div className={styles.UserInfo__initials}>KS</div>
-        }
-        <div className={styles.UserInfo__onlineDot}></div>
+      <div className={styles.UserInfo__content}>
+        <div className={styles.UserInfo__image}>
+          {img ?
+            <img src={img} alt="Profile" />
+            :
+            <div className={styles.UserInfo__initials}>KS</div>
+          }
+          <div className={styles.UserInfo__onlineDot}></div>
+        </div>
+        <div className={styles.UserInfo__text}>
+          <div className={styles.UserInfo__name}>
+            Pawan Kolhe
+          </div>
+          <div className={styles.UserInfo__email}>
+            contact@pawankolhe.com
+          </div>
+        </div>
       </div>
-      <div className={styles.UserInfo__text}>
-        <div className={styles.UserInfo__name}>
-          Pawan Kolhe
-        </div>
-        <div className={styles.UserInfo__email}>
-          contact@pawankolhe.com
-        </div>
+      <div className={styles.UserInfo__icon}>
+        <InlineIcon icon="akar-icons:chevron-down" />
       </div>
     </div>
   )
